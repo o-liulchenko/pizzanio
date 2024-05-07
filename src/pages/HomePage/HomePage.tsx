aaf8f29d0af08c776356e9d6000817b49b6c68cc
o-liulchenko <o.liulchenko@ukr.net>
2024-05-07 12:54:41 +0300

    some changes

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -5,7 +5,9 @@ import styles from './HomePage.module.css';
 export const HomePage:FC = () => {
   return (
     <>
-      
+      <div className={styles.homePage}>
+        HomePage
+      </div>
     </>
   );
 };
