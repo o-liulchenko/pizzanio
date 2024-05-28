67499cf7de68b5a1e6051d0e32790676e34b810f
o-liulchenko <o.liulchenko@ukr.net>
2024-05-28 23:43:24 +0300

    edit db.json

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,13 +1,47 @@
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
+import axios from "axios";
+import styles from "./HomePage.module.css";
 
-import styles from './HomePage.module.css';
+interface IData {
+  description: string;
+  discount: number;
+  image: string;
+  name: string;
+  price: number;
+}
 
-export const HomePage:FC = () => {
+export const HomePage: FC = () => {
+  let [sales, setSales] = useState<IData[]>([]);
+  let [isloading, setIsloading] = useState(true);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3030/pizzas/discount")
+      .then((response) => {
+        setSales(response.data);
+        setIsloading(false);
+      });
+  }, []);
+
   return (
     <>
-      <div className={styles.homePage}>
-        HomePage
-      </div>
+      <section className={styles.mainPageContainer}>
+        <article className={styles.description}>
+          <h1 className={styles.descriptionTitle}>Pizza</h1>
+          <h2 className={styles.descriptionSubitle}>Special</h2>
+        </article>
+        <article>
+          {
+            isloading
+            ? <span>loading</span>
+            : sales.map(e => <>
+              <p>{e.name}</p>
+              <p>{e.price} uah.</p>
+              <img src={e.image} alt="" />
+            </>)
+          }
+        </article>
+      </section>
     </>
   );
 };
--- src/pages/MenuPage/MenuPage.tsx
+++ src/pages/MenuPage/MenuPage.tsx
@@ -1,6 +1,18 @@
-import { FC } from "react";
+import axios from "axios";
+import { FC, useEffect, useState } from "react";
 
-export const MenuPage:FC = () => {
+export const MenuPage: FC = () => {
+  let [sales, setSales] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://my-json-server.typicode.com/o-liulchenko/pizzanio/sales")
+      .then((response) => {
+        console.log(response.data);
+        setSales(response.data);
+      });
+  }, []);
+
   return (
     <>
       <h1>MenuPage</h1>
